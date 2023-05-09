b557456acb24db378c747702e49ec6bd54ee6a97
Gabriel Gomes <gomes@berkeley.edu>
2023-05-08 18:54:19 -0700

    small

--- src/otm-queue/test.py
+++ src/otm-queue/test.py
@@ -1,8 +1,5 @@
 import unittest
-import numpy as np
-import matplotlib.pyplot as plt
 from core import Scenario
-import heapq
 
 class MyTestCase(unittest.TestCase):
 
@@ -14,57 +11,15 @@ class MyTestCase(unittest.TestCase):
             output_folder = '../../output',
             prefix = 'run1',
             requests = [
-                { 'type':'link_flw', 'dt':'10','links':'1,2,3,4,5,6,7,8' },
-                { 'type':'link_veh', 'dt':'10', 'links':'1,2,3,4,5,6,7,8' },
-                { 'type':'lg_flw', 'dt':'10', 'links':'1,2,3,4,5,6,7,8' },
-                { 'type':'lg_veh', 'dt':'10', 'links':'1,2,3,4,5,6,7,8' },
-                { 'type':'veh_events', 'links':'1,2,3,4,5,6,7,8' },
+                # { 'type':'link_flw', 'dt':'10','links':'1,2,3,4,5,6,7,8' },
+                # { 'type':'link_veh', 'dt':'1', 'links':'1,2,3,4,5,6,7,8' },
+                # { 'type':'lg_flw', 'dt':'10', 'links':'1,2,3,4,5,6,7,8' },
+                # { 'type':'lg_veh', 'dt':'10', 'links':'1,2,3,4,5,6,7,8' },
+                # { 'type':'veh_events', 'links':'1,2,3,4,5,6,7,8' },
                 { 'type':'cnt_events', 'ids':'0' }
             ]
         )
 
         scenario.initialize(output_prefix='out/a',validate=True)
-        scenario.run(duration=100)
+        scenario.run(duration=500)
         self.assertTrue(True)
-
-    def test_priority_queue(self) -> None:
-
-        from queue import PriorityQueue
-
-        q:PriorityQueue = PriorityQueue()
-        q.put(10)
-        q.put(1)
-        q.put(5)
-        while not q.empty():
-            print(q.get())
-        self.assertTrue(True)
-
-    def test_heapqueue(self) -> None:
-        a = [(1,"asgs"),(7,"ro320("),(-1,"sdsfd("),(2,"ddddd(")]
-        heapq.heapify(a)
-
-        print("heapify:")
-        print(a)
-
-        print("pop:")
-        print(heapq.heappop(a))
-        print(a)
-
-
-        print("remove 2:")
-        a.remove((2,"ddddd("))
-        print(a)
-
-        print("pop:")
-        print(heapq.heappop(a))
-        print(a)
-
-    def test_random_choice(self) -> None:
-        N = 100
-        A = np.empty(N)
-        for i in range(N):
-            A[i] = np.random.choice([0,1,-5,10,8], p=[0.1, 0, 0.3, 0.6, 0])
-
-        # plt.figure()
-        # plt.hist(A)
-        # plt.show()
